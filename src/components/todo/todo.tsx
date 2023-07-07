b1f8fbec976e66893b3b9e5eb79ba3c10ce773b2
Jordan Fox <jordan.fox@canstar.com.au>
2023-07-07 18:31:13 +1000

    boostrapping up with vite and vitest

---
+++ src/components/todo/todo.tsx
@@ -0,0 +1,10 @@
+export function Todo() {
+
+  return (
+    <div className="todo">
+      <input placeholder="todo" type="text" />
+      <button>edit</button>
+      <button>delete</button>
+    </div>
+  );
+}
